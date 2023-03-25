312088c9d404464415b7065fd025671bc3098883
carlagesa <carltonmbaja0@gmail.com>
2023-03-25 11:29:39 +0300

    ../

--- cocktaildb/settings.py
+++ cocktaildb/settings.py
@@ -37,6 +37,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'staticfiles',
     'rest_framework',
     'cocktail',
 ]
@@ -118,6 +119,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.1/howto/static-files/
 
 STATIC_URL = 'static/'
+STATICFILES_DIRS = [os.path.join(BASE_DIR, "static")]
 STATIC_ROOT = os.path.join(BASE_DIR, "staticfiles")
 
 # Default primary key field type
