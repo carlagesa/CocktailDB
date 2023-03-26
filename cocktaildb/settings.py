239ab2de66171e8b0b3b7a2780e189fdafa14b9b
carlagesa <carltonmbaja0@gmail.com>
2023-03-26 11:44:28 +0300

    Switched from cors-headers to cors-middleware

--- cocktaildb/settings.py
+++ cocktaildb/settings.py
@@ -44,7 +44,8 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'corsheaders',
+    'django-cors-middleware',
+    # 'corsheaders',
     'django_filters',
     'rest_framework',
     'cocktail',
@@ -53,8 +54,8 @@ INSTALLED_APPS = [
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
-    "corsheaders.middleware.CorsMiddleware",
     'django.contrib.sessions.middleware.SessionMiddleware',
+    'corsheaders.middleware.CorsMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
     'django.contrib.auth.middleware.AuthenticationMiddleware',
