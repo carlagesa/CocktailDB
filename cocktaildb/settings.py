831e048bf7a49b55b9ce3a109abcf41e464e34c6
carlagesa <carltonmbaja0@gmail.com>
2023-03-26 10:44:10 +0300

    Fix CORS error

--- cocktaildb/settings.py
+++ cocktaildb/settings.py
@@ -40,6 +40,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'corsheaders',
     'django_filters',
     'rest_framework',
     'cocktail',
