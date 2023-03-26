71a2acf68bd7ecc3cb4c3f044facb40702b33fa1
carlagesa <carltonmbaja0@gmail.com>
2023-03-26 10:51:47 +0300

    CORS

--- cocktaildb/settings.py
+++ cocktaildb/settings.py
@@ -27,7 +27,8 @@ DEBUG = True
 
 CSRF_TRUSTED_ORIGINS = ['https://cocktaildb.up.railway.app']
 
-CORS_ALLOWED_ORIGINS = ['*']
+# CORS_ALLOWED_ORIGINS = ['*']
+CORS_ORIGIN_ALLOW_ALL = True
 
 ALLOWED_HOSTS = ['cocktaildb.up.railway.app']
 
