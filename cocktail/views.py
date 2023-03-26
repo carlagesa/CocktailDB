68b31cef2e93f9f44bbf9900f6236e98ca2d9a5a
carlagesa <carltonmbaja0@gmail.com>
2023-03-26 10:22:50 +0300

    add multiple search parameters to the Swagger doc

--- cocktail/views.py
+++ cocktail/views.py
@@ -2,12 +2,15 @@ from django_filters.rest_framework import DjangoFilterBackend
 from rest_framework import generics, filters
 from cocktail.models import Cocktail
 from cocktail.serializers import CocktailSerializer
+from drf_yasg import openapi
+from drf_yasg.utils import swagger_auto_schema
+
 
 class CocktailList(generics.ListCreateAPIView):
     queryset = Cocktail.objects.all()
     serializer_class = CocktailSerializer
     filter_backends = [filters.SearchFilter]
-    search_fields = ['drink', 'category','Ingredient1']
+    search_fields = ['drink', 'category', 'Ingredient1']
     filter_backends = [DjangoFilterBackend]
     filterset_fields = {
         'alcoholic': ['exact'],
@@ -15,7 +18,31 @@ class CocktailList(generics.ListCreateAPIView):
         'glass': ['exact'],
     }
 
+    @swagger_auto_schema(
+        manual_parameters=[
+            openapi.Parameter(
+                name="cocktail",
+                in_=openapi.IN_QUERY,
+                type=openapi.TYPE_STRING,
+                description="Search by name of the cocktail",
+            ),
+            openapi.Parameter(
+                name="glass",
+                in_=openapi.IN_QUERY,
+                type=openapi.TYPE_STRING,
+                description="Search by type of glass",
+            ),
+            openapi.Parameter(
+                name="drink",
+                in_=openapi.IN_QUERY,
+                type=openapi.TYPE_STRING,
+                description="Search by type of drink",
+            ),
+        ]
+    )
+    def get(self, request, *args, **kwargs):
+        return super().get(request, *args, **kwargs)
+
 class CocktailDetail(generics.RetrieveUpdateDestroyAPIView):
     queryset = Cocktail.objects.all()
     serializer_class = CocktailSerializer
-
