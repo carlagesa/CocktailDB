43fe610cdbfd10356dd8e09848d591cced1a3aaf
carlton <carltonagesa@gmail.com>
2024-08-06 10:07:20 +0300

    Create the Drink Model

--- cocktail/drinks/models.py
+++ cocktail/drinks/models.py
@@ -1,3 +1,12 @@
 from django.db import models
 
-# Create your models here.
+class Drink(models.Model):
+    name = models.CharField(max_length=100)
+    category = models.CharField(max_length=100)
+    alcoholic = models.CharField(max_length=10)
+    glass = models.CharField(max_length=100)
+    instructions = models.TextField()
+    image_url = models.URLField(max_length=200)
+
+    def __str__(self):
+        return self.name
