28c8c9c2af3cd20a13c7e3bb7b6ca08359a5fad1
carlton <carltonagesa@gmail.com>
2024-08-06 10:22:03 +0300

    Create API Endpoints

--- cocktail/drinks/views.py
+++ cocktail/drinks/views.py
@@ -1,3 +1,7 @@
-from django.shortcuts import render
+from rest_framework import viewsets
+from .models import Drink
+from .serializers import DrinkSerializer
 
-# Create your views here.
+class DrinkViewSet(viewsets.ModelViewSet):
+    queryset = Drink.objects.all()
+    serializer_class = DrinkSerializer
