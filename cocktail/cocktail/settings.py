64c13586ba7b9be0b65274cdc83368207998dad6
carlton <carltonagesa@gmail.com>
2024-08-09 09:50:34 +0300

    Deploy change

--- cocktail/cocktail/settings.py
+++ cocktail/cocktail/settings.py
@@ -1,3 +1,4 @@
+import os 
 from pathlib import Path
 from datetime import timedelta
 from django.conf import settings
@@ -121,10 +122,21 @@ WSGI_APPLICATION = 'cocktail.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/5.0/ref/settings/#databases
 
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': BASE_DIR / 'db.sqlite3',
+#     }
+# }
+
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': os.environ.get("DB_NAME"),
+        'USER': os.environ.get("DB_USER"),
+        'PASSWORD': os.environ.get("DB_PASSWORD"),
+        'HOST': os.environ.get("DB_HOST"),
+        'PORT': os.environ.get("DB_PORT"),
     }
 }
 
