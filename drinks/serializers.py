32ea9db0c37a2d74e3218368fd2d05f447fd53fa
carlton <carltonagesa@gmail.com>
2024-08-24 18:20:56 +0300

    V1 Launch

--- drinks/serializers.py
+++
@@ -1,7 +0,0 @@
-from rest_framework import serializers
-from .models import Drink
-
-class DrinkSerializer(serializers.ModelSerializer):
-    class Meta:
-        model = Drink
-        fields = '__all__'
