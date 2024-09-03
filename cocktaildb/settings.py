45af41f50476f96865ea47354598129dae432157
Carlton Agesa <65759360+carlagesa@users.noreply.github.com>
2024-09-03 10:41:29 +0300

    DB config

--- cocktaildb/settings.py
+++ cocktaildb/settings.py
@@ -113,7 +113,7 @@ WSGI_APPLICATION = 'cocktaildb.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'ENGINE': 'django.db.backends.postgresql',
         'NAME': os.environ.get("DB_NAME"),
         'USER': os.environ.get("DB_USER"),
         'PASSWORD': os.environ.get("DB_PASSWORD"),
