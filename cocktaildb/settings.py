f6437452df75377f55ec661b6999e4f05dce75ce
Calton Agesa <carltonmbaja0@gmail.com>
2025-02-13 00:52:56 +0300

    resume auth

--- cocktaildb/settings.py
+++ cocktaildb/settings.py
@@ -33,7 +33,7 @@ ALLOWED_HOSTS =  ["127.0.0.1", ".vercel.app"]
 
 INSTALLED_APPS = [
     # 'django.contrib.admin',
-    # 'django.contrib.auth',
+    'django.contrib.auth',
     'django.contrib.contenttypes',
     'django.contrib.sessions',
     'django.contrib.messages',
@@ -49,7 +49,7 @@ MIDDLEWARE = [
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
-    # 'django.contrib.auth.middleware.AuthenticationMiddleware',
+    'django.contrib.auth.middleware.AuthenticationMiddleware',
     # 'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
 ]
