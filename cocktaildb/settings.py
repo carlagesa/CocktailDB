0fcc3f1537e0d06f5f0f8263b262edb038ce0d7a
Calton Agesa <carltonmbaja0@gmail.com>
2025-02-13 00:24:28 +0300

    Add static

--- cocktaildb/settings.py
+++ cocktaildb/settings.py
@@ -49,7 +49,7 @@ MIDDLEWARE = [
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
-    # 'django.contrib.auth.middleware.AuthenticationMiddleware',
+    'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
 ]
