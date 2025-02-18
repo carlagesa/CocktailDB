63d5380c1e4b5da22e53dc280ee2a73ebc9ad41a
Calton Agesa <carltonmbaja0@gmail.com>
2025-02-18 17:49:41 +0300

    Update static settings

---
+++ Dockerfile
@@ -0,0 +1,24 @@
+# Use the official Python image from Docker Hub
+FROM python:3.9-slim
+
+# Set the working directory inside the container
+WORKDIR /cocktail-app
+
+# Copy the requirements file into the container
+COPY requirements.txt /cocktail-app/
+
+# Install the dependencies from the requirements file
+RUN pip install --no-cache-dir -r requirements.txt
+
+# Copy the entire project into the container
+COPY . /cocktail-app/
+
+# Expose the port the app will run on (e.g., 8000 for Django)
+EXPOSE 8000
+
+# Run the application (adjust to how you start your Django app)
+# CMD ["python", "manage.py", "runserver", "0.0.0.0:8000"]
+
+#Adjusting for Production!
+CMD ["gunicorn", "--bind", "0.0.0.0:8000", "cocktaildb.wsgi:application"] 
+
--- cocktaildb/settings.py
+++ cocktaildb/settings.py
@@ -23,9 +23,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-&0u0yhr%m%sx7temeq8zuo$3h3-@f=gs5vh-$65a8njs3*qs#$'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
-ALLOWED_HOSTS =  ['.elasticbeanstalk.com', '172.31.42.170']
+ALLOWED_HOSTS =  ['docker-env.eba-df3dpndr.eu-west-2.elasticbeanstalk.com']
 
 
 
@@ -105,21 +105,14 @@ WSGI_APPLICATION = 'cocktaildb.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/5.0/ref/settings/#databases
 
-# DATABASES = {
-#     'default': {
-#         'ENGINE': 'django.db.backends.sqlite3',
-#         'NAME': BASE_DIR / 'db.sqlite3',
-#     }
-# }
-
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.postgresql',  # Change to 'django.db.backends.mysql' if using MySQL
-        'NAME': os.getenv('DB_NAME'),
-        'USER': os.getenv('DB_USER'),
-        'PASSWORD': os.getenv('DB_PASSWORD'),
-        'HOST': os.getenv('DB_HOST'),
-        'PORT': os.getenv('DB_PORT', '5432'),  # Default PostgreSQL port
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': os.environ.get("DB_NAME"),
+        'USER': os.environ.get("DB_USER"),
+        'PASSWORD': os.environ.get("DB_PASSWORD"),
+        'HOST': os.environ.get("ep-falling-wildflower-a4im662j-pooler.us-east-1.aws.neon.tech"),
+        'PORT': '5432',
     }
 }
 
@@ -159,10 +152,7 @@ USE_TZ = True
 
 # Static files (CSS, JavaScript, Images)
 STATIC_URL = '/static/'
-STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
-
-MEDIA_URL = '/media/'
-MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
+STATIC_ROOT = 'static'
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.0/ref/settings/#default-auto-field
 
