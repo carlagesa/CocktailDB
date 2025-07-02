cdda9785ed1939b9503ab1e48032d3e45cb1e2fc
carl-dev <carltonmbaja0@gmail.com>
2025-07-03 01:35:57 +0300

    fix: change version of the AWS provider

--- terraform/main.tf
+++ terraform/main.tf
@@ -2,11 +2,9 @@ terraform {
   required_providers {
     aws = {
       source  = "hashicorp/aws"
-      version = ">= 3.0"
+      version = "~> 5.40.0"
     }
   }
-
-  required_version = ">= 1.0"
 }
 
 provider "aws" {
