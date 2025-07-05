0b5f4bef4925a6a7b65fb4a590a8d6c6678f2079
carl-dev <carltonmbaja0@gmail.com>
2025-07-05 13:35:59 +0300

    fix: Add RDS variables for database configuration in Terraform

--- terraform/variables.tf
+++ terraform/variables.tf
@@ -3,3 +3,28 @@ variable "region" {
   default     = "us-east-1"
 }
 
+variable "image_tag" {
+  description = "Tag of the Docker image to deploy"
+  type        = string
+}
+
+variable "rds_hostname" {
+  description = "The hostname of the RDS instance"
+  type        = string
+}
+
+variable "rds_db_name" {
+  description = "Name of the RDS database"
+  type        = string
+}
+
+variable "rds_username" {
+  description = "Username for the RDS database"
+  type        = string
+}
+
+variable "rds_password" {
+  description = "Password for the RDS database"
+  type        = string
+  sensitive   = true
+}
